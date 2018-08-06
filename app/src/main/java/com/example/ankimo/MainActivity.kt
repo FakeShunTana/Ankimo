32cb12a1a7aa95b300a30392a4a76a13198a893e
Shunsaku Tanabe <shunsakutanabe@gmail.com>
2018-08-06 22:47:50 +0900

    Change

--- app/src/main/java/com/example/ankimo/MainActivity.kt
+++ app/src/main/java/com/example/ankimo/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.ankimo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAdView : AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MobileAds.initialize(this, "ca-app-pub-4235083772211538~8348710579")
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
 }
